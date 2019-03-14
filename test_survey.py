f2ae3663efd732529c7d718d8210463eda2d3262
up-everyday <44463130+up-everyday@users.noreply.github.com>
2019-03-14 17:54:54 +0800

    Create test_survey.py

---
+++ test_survey.py
@@ -0,0 +1,25 @@
+import unittest
+from survey import AnonymousSurvey
+#from hello import AnonymousSurvey
+
+class TestAnonymousSurvey(unittest.TestCase):
+    """针对AnonymousSurvey类的测试"""
+    def setUp(self):
+        """创建一个测试对象和测试结果"""
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Spanish', 'Mandarin']
+
+    def test_store_single_response(self):
+        """测试单个答案会被妥善保存"""
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
+
+    def test_store_three_response(self):
+        """测试三个答案会被妥善保存"""
+        for response in self.responses:
+            self.my_survey.store_response(response)
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
+if __name__ == '__main__':
+    unittest.main()
